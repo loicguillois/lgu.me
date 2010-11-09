a507f825520deb74bcf3cffa1d5641d70dd85806
Loic GUILLOIS <contact@loicguillois.fr>
2010-11-09 17:31:22 +0100

    adding the uuid function

--- server.js
+++ server.js
@@ -21,11 +21,11 @@ function post(request, response) {
 response.writeHead(200, { 'Content-Type' : 'text/html' });
 
       response.write(
-		  'URL: <strong>' + request_data.url + '</strong><br />'
+		  'URL: <strong>' + request_data.url + '</strong><br />'+
+		  'Key: <strong>' + createUUID() + '</strong><br />'
                 );
 		response.end();
 	    });
-
 }
 
 function post_handler(request, callback)
@@ -55,6 +55,16 @@ function redirect(request, response) {
   response.end(uri+'\n');
 }
 
+function createUUID() {
+    var s = [];
+    var chars = "azertyuiopqsdfghjklmwxcvbn";
+    chars = chars + chars.toUpperCase();
+    for (var i = 0; i < 6; i++) {
+        s[i] = chars.substr(Math.floor(Math.random() * 52), 1);
+    }
+    var uuid = s.join("");
+    return uuid;
+}
 
 
 console.log('Server running at http://127.0.0.1:8124/');
