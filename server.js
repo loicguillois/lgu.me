44b14e1deabf95540dd9618ce2fe7e227cdee234
Loic GUILLOIS <contact@loicguillois.fr>
2010-11-09 17:21:56 +0100

    form is working

--- server.js
+++ server.js
@@ -1,10 +1,60 @@
 var http = require('http'),
-    url = require("url");
+    url = require("url"),
+    sys = require('sys'),
+    querystring = require('querystring');
+
 
 var server = http.createServer(function (request, response) {
-  var uri = url.parse(request.url).pathname; 
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end(uri+'\n');
+  switch (url.parse(request.url).pathname) {
+    case '/post':
+      post(request, response);
+      break;
+    default:
+      redirect(request, response);
+      break;
+  }
 }).listen(8124);
+
+function post(request, response) {
+  post_handler(request, function(request_data)
+    {
+response.writeHead(200, { 'Content-Type' : 'text/html' });
+
+      response.write(
+		  'URL: <strong>' + request_data.url + '</strong><br />'
+                );
+		response.end();
+	    });
+
+}
+
+function post_handler(request, callback)
+{
+    var _REQUEST = { };
+    var _CONTENT = '';
+
+    if (request.method == 'POST')
+    {
+        request.addListener('data', function(chunk)
+	{
+	    _CONTENT+= chunk;
+	});
+
+	request.addListener('end', function()
+	{
+            _REQUEST = querystring.parse(_CONTENT);
+	    callback(_REQUEST);
+	});
+    };
+};
+
+
+function redirect(request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  var uri = url.parse(request.url).pathname; 
+  response.end(uri+'\n');
+}
+
+
 
 console.log('Server running at http://127.0.0.1:8124/');
